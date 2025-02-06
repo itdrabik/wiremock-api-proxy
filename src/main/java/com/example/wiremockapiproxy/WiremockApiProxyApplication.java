46ca2a7081040046a42786041974f32672f550a9
Kamil Drabik <it.drabik@gmail.com>
2025-02-06 13:42:21 +0100

    Spring Boot project initialization

---
+++ src/main/java/com/example/wiremockapiproxy/WiremockApiProxyApplication.java
@@ -0,0 +1,11 @@
+package com.example.wiremockapiproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockApiProxyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WiremockApiProxyApplication.class, args);
+    }
+}
